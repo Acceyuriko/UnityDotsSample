cddbc92140ba90ea43c0d2f8bca016226c7dfb7d
Acceyuriko <Acceyuriko@gmail.com>
2022-02-24 18:22:31 +0800

    feat: styled TitleScreenManager

--- Assets/UI/ManualConnectScreen.cs
+++ Assets/UI/ManualConnectScreen.cs
@@ -1,17 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
-public class ManualConnectScreen: VisualElement
+public class ManualConnectScreen : VisualElement
 {
+    TextField m_GameIp;
+    TextField m_PlayerName;
+    string m_HostName = "";
+    IPAddress m_MyIp;
+
     public new class UxmlFactory : UxmlFactory<ManualConnectScreen, UxmlTraits> { }
 
     public ManualConnectScreen()
     {
-        this.RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+        this.RegisterCallback<GeometryChangedEvent>(OnGeometryChange);
     }
 
-    void OnGeometryChanged(GeometryChangedEvent evt)
+    void OnGeometryChange(GeometryChangedEvent evt)
     {
-        this.UnregisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+        // 
+        // PROVIDE ACCESS TO THE FORM ELEMENTS THROUGH VARIABLES
+        // 
+        m_GameIp = this.Q<TextField>("game-ip");
+        m_PlayerName = this.Q<TextField>("player-name");
+
+        //  CLICKING CALLBACKS
+        this.Q("launch-connect-game")?.RegisterCallback<ClickEvent>(ev => ClickedJoinGame());
+
+        this.UnregisterCallback<GeometryChangedEvent>(OnGeometryChange);
     }
+
+    void ClickedJoinGame()
+    {
+        Debug.Log("clicked manual connect");
+    }
+
 }
--- Assets/UI/TitleScreenManager.cs
+++ Assets/UI/TitleScreenManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,30 +9,30 @@ public class TitleScreenManager : VisualElement
     VisualElement m_HostScreen;
     VisualElement m_JoinScreen;
     VisualElement m_ManualConnectScreen;
-
+    
     public new class UxmlFactory : UxmlFactory<TitleScreenManager, UxmlTraits> { }
 
     public TitleScreenManager()
     {
-        this.RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+        this.RegisterCallback<GeometryChangedEvent>(OnGeometryChange);
     }
 
-    void OnGeometryChanged(GeometryChangedEvent evt)
+    void OnGeometryChange(GeometryChangedEvent evt)
     {
         m_TitleScreen = this.Q("TitleScreen");
         m_HostScreen = this.Q("HostGameScreen");
         m_JoinScreen = this.Q("JoinGameScreen");
         m_ManualConnectScreen = this.Q("ManualConnectScreen");
 
-        m_TitleScreen?.Q("host-local-button")?.RegisterCallback<ClickEvent>(ev => EnableHostScreen());
-        m_TitleScreen?.Q("join-local-button")?.RegisterCallback<ClickEvent>(ev => EnableJoinScreen());
-        m_TitleScreen?.Q("manual-connect-button")?.RegisterCallback<ClickEvent>(ev => EnableManualScreen());
+        m_TitleScreen?.Q("host-local-game")?.RegisterCallback<ClickEvent>(ev => EnableHostScreen());
+        m_TitleScreen?.Q("join-local-game")?.RegisterCallback<ClickEvent>(ev => EnableJoinScreen());
+        m_TitleScreen?.Q("manual-connect")?.RegisterCallback<ClickEvent>(ev => EnableManualScreen());
 
         m_HostScreen?.Q("back-button")?.RegisterCallback<ClickEvent>(ev => EnableTitleScreen());
         m_JoinScreen?.Q("back-button")?.RegisterCallback<ClickEvent>(ev => EnableTitleScreen());
         m_ManualConnectScreen?.Q("back-button")?.RegisterCallback<ClickEvent>(ev => EnableTitleScreen());
 
-        this.UnregisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+        this.UnregisterCallback<GeometryChangedEvent>(OnGeometryChange);
     }
 
     public void EnableHostScreen()
@@ -39,10 +41,12 @@ public class TitleScreenManager : VisualElement
         m_HostScreen.style.display = DisplayStyle.Flex;
         m_JoinScreen.style.display = DisplayStyle.None;
         m_ManualConnectScreen.style.display = DisplayStyle.None;
+
     }
 
     public void EnableJoinScreen()
     {
+        Debug.Log("Enable join screen trigger");
         m_TitleScreen.style.display = DisplayStyle.None;
         m_HostScreen.style.display = DisplayStyle.None;
         m_JoinScreen.style.display = DisplayStyle.Flex;
@@ -64,4 +68,5 @@ public class TitleScreenManager : VisualElement
         m_JoinScreen.style.display = DisplayStyle.None;
         m_ManualConnectScreen.style.display = DisplayStyle.None;
     }
+
 }
